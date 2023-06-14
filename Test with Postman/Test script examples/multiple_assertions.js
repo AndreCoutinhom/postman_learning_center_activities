0a3523949a3df0742c786a003e80f7b4c6fdbe0b
André Coutinho de Macedo Silva <91290799+AndreCoutinhom@users.noreply.github.com>
2023-06-14 09:23:31 -0300

    Create multiple_assertions.js

---
+++ Test with Postman/Test script examples/multiple_assertions.js
@@ -0,0 +1,7 @@
+pm.test("The response has all properties", () => {
+    //parse the response JSON and test three properties
+    const responseJson = pm.response.json();
+    pm.expect(responseJson.type).to.eql('vip');
+    pm.expect(responseJson.name).to.be.a('string');
+    pm.expect(responseJson.id).to.have.lengthOf(1);
+});
